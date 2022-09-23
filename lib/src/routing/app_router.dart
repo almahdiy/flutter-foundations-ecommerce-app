6423fbf87adf2f39cb4611e8c4f9ebec2e2dfe91
almahdiy <yaqeen.a.almahdi@gmail.com>
2022-09-23 21:04:01 +0300

    Using name routes

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,25 +1,62 @@
+import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/products_list/products_list_screen.dart';
 import '../features/shopping_cart/shopping_cart_screen.dart';
 
+enum AppRoute { home, cart, orders, account, signIn }
+
 final goRouter = GoRouter(
   initialLocation: '/',
   debugLogDiagnostics: true,
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const ProductsListScreen(), routes: [
-      GoRoute(
-        path: 'cart',
-        // builder: (context, state) => const ShoppingCartScreen(),
-        // We replaced the builder argument with pageBuilder so we can use fullscreenDialog. This control the transition into the new route.
-        // with builder, the transition was right to left. With pageBuilder + fullscreenDialog, the transition is bottom-up.
-        // This also turns the back button into a close button (<- to X).
-        pageBuilder: (context, state) => const MaterialPage(
-          child: const ShoppingCartScreen(),
-          fullscreenDialog: true,
-        ),
-      ),
-    ]),
+    GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsListScreen(),
+        name: AppRoute.home.name,
+        routes: [
+          GoRoute(
+            path: 'cart',
+            name: AppRoute.cart.name,
+            // builder: (context, state) => const ShoppingCartScreen(),
+            // We replaced the builder argument with pageBuilder so we can use fullscreenDialog. This control the transition into the new route.
+            // with builder, the transition was right to left. With pageBuilder + fullscreenDialog, the transition is bottom-up.
+            // This also turns the back button into a close button (<- to X).
+            pageBuilder: (context, state) => const MaterialPage(
+              child: const ShoppingCartScreen(),
+              fullscreenDialog: true,
+            ),
+          ),
+          GoRoute(
+            path: 'orders',
+            name: AppRoute.orders.name,
+            pageBuilder: (context, state) => const MaterialPage(
+              child: const OrdersListScreen(),
+              fullscreenDialog: true,
+            ),
+          ),
+          GoRoute(
+            path: 'account',
+            name: AppRoute.account.name,
+            pageBuilder: (context, state) => const MaterialPage(
+              child: const AccountScreen(),
+              fullscreenDialog: true,
+            ),
+          ),
+          GoRoute(
+            path: 'signIn',
+            name: AppRoute.signIn.name,
+            pageBuilder: (context, state) => const MaterialPage(
+              child: EmailPasswordSignInScreen(
+                formType: EmailPasswordSignInFormType.signIn,
+              ),
+              fullscreenDialog: true,
+            ),
+          ),
+        ]),
   ],
 );
