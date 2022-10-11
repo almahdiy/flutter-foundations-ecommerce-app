7507a3bb87ed2a1b94f831c968a2ed0ea3a7065e
almahdiy <yaqeen.a.almahdi@gmail.com>
2022-10-11 14:07:47 +0300

    creating repositories using abstract classes

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,28 +1,78 @@
+//https://codewithandrea.com/articles/flutter-repository-pattern/
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../domain/app_user.dart';
 
-class FakeAuthRepository {
+// Use an abstract class to be able to easily switch between auth repositories based on environment variables
+abstract class AuthRepository {
+  Stream<AppUser?> authStateChanges();
+  AppUser? get currentUser;
+  Future<void> signInWithEmailAndPassword(String email, String password);
+  Future<void> createUserWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+}
+
+class FirebaseAuthRepository implements AuthRepository {
+  @override
+  Stream<AppUser?> authStateChanges() {
+    // TODO: implement authStateChanges
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> createUserWithEmailAndPassword(String email, String password) {
+    // TODO: implement createUserWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AppUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
+
+class FakeAuthRepository implements AuthRepository {
   // To check the authentication state of the user
+  @override
   Stream<AppUser?> authStateChanges() => Stream.value(null); //TODO: Implememt
 
+  @override
   AppUser? get currentUser => null; // TODO: Implement
 
+  @override
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     // TODO: Implement
   }
 
+  @override
   Future<void> createUserWithEmailAndPassword(String email, String password) async {
     // TODO: Implement
   }
 
+  @override
   Future<void> signOut() async {
     // TODO: Implement
   }
 }
 
-final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
-  return FakeAuthRepository();
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  // The following can be be set by running the app with the following flag: --dart-define=useFakeRepos=true
+  // final isFake = String.fromEnvironment("userFaleRepos") == 'true';
+  const isFake = true;
+  return isFake ? FakeAuthRepository() : FakeAuthRepository();
+  // This allows us to easily switch between the FakeAuthRepository and Firebase without having to make any changes to the codebase.
 });
 
 final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
